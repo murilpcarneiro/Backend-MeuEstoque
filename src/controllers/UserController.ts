300bf31dd8f74647299e785a3bd5c45d4036b236
Murilo <murilpcarneiro@gmail.com>
2025-08-04 17:42:05 -0300

    feat: adiciona lógica de jwt token e adiciona tratamento de erros no register

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,6 +3,16 @@ import * as UserService from "../services/UserService";
 
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
-  const user = await UserService.createUser(name, email, password);
-  res.status(201).json(user);
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "Name, email, and password are required." });
+  }
+
+  try {
+    const { user, token } = await UserService.createUser(name, email, password);
+    res.status(201).json({ token, name: user.name, email: user.email });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while creating the user." });
+  }
+
 };
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,15 +1,17 @@
+import bycrypt from 'bcrypt';
 import { db } from 'db/config/db';
+import jwt from 'jsonwebtoken';
 import { users } from 'models/User';
 
+const JWT_SECRET = process.env.JWT_SECRET || 'default';
+
 export const createUser = async (name: string, email: string, password: string) => {
-  try {
-    return await db.insert(users).values({
-      name,
-      email,
-      password,
-    }).returning();
-    
-  } catch (error: any) {
-    throw new Error(`Failed to create user: ${error.message}`);
-  }
+  const hash = await bycrypt.hash(password, 10);
+  const [user] = await db.insert(users).values({
+    name,
+    email,
+    password: hash,
+  }).returning();
+  const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '1h'});
+  return {user, token}
 }
